30749d81bdd898ffff0c4114401fe342abca82fc
jakewex <jmw355@georgetown.edu>
2021-09-16 12:07:49 -0400

    Update game.py

--- game.py
+++ game.py
@@ -4,10 +4,10 @@ print("Rock, Paper, Scissors, Shoot!")
 
 #PROMPT USER FOR INPUT
 usermove = input("Game on! Choose 'rock' or 'paper' or 'scissors'!")
-print(usermove)
+print("The user chose", usermove)
 
 #DETERMINE CPU CHOICE Randomly
 options = ["rock", "paper", "scissors"]
 random.shuffle(options)
 cpumove = random.choice(options)
-print("Computer chose", cpumove)
+print("The computer chose", cpumove)
