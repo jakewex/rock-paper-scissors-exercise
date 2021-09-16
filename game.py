252c91e8437616a04ce54f00b9eeb327c180ed76
jakewex <jmw355@georgetown.edu>
2021-09-16 12:03:12 -0400

    Update game.py

--- game.py
+++ game.py
@@ -1,25 +1,14 @@
 #game.py
-
+import random
 print("Rock, Paper, Scissors, Shoot!")
 
 #PROMPT USER FOR INPUT
 usermove = input("Game on! Choose 'rock' or 'paper' or 'scissors'!")
 print(usermove)
 
-if usermove == "rock":
-    print("The computer chose paper")
-    print("Paper beats rock")
-    print("You lost to the computer")
-    print("Please play again!")
-elif usermove == "paper":
-    print("The computer chose scissors")
-    print("Scissors beats paper")
-    print("You lost to the computer")
-    print("Please play again!")  
-elif usermove == "scissors":
-    print("The computer chose rock")
-    print("Rock beats scissors")
-    print("You lost to the computer")
-    print("Please play again!")  
-else:
-    print("Please play again, schoosing either rock, paper, or scissors")
+#DETERMINE CPU CHOICE
+options = ["rock", "paper", "scissors"]
+random.shuffle(options)
+cpumove = random.choice(options)
+print("Computer chose")
+print(cpumove)
