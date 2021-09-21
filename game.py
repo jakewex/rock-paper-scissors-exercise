ce08cf9eeb22dfe6c0702563b8e0db4fe698d112
jakewex <jmw355@georgetown.edu>
2021-09-21 11:14:38 -0400

    Update game.py

--- game.py
+++ game.py
@@ -5,14 +5,18 @@ print("Rock, Paper, Scissors, Shoot!")
 #PROMPT USER FOR INPUT
 usermove = input("Game on! Choose 'rock' or 'paper' or 'scissors'!")
 print("The user chose", usermove)
+options = ["rock", "paper", "scissors"]
+if usermove not in options:
+    print("User did not make a valid choice")
+    print("Try again, selecting either 'rock', 'paper', or 'scissors'")
 
 #DETERMINE CPU CHOICE Randomly
-options = ["rock", "paper", "scissors"]
 random.shuffle(options)
 cpumove = random.choice(options)
 print("The computer chose", cpumove)
 
 #DETERMINE WINNER BASED ON CHOICES
+
 if cpumove == usermove:
     print("User and computer both chose", usermove)
     print("Please try again to determine a winner")
