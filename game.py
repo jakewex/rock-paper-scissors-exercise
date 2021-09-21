ec89d501c3ed4a1dbc1f2415fce32671d9045901
jakewex <jmw355@georgetown.edu>
2021-09-21 11:15:41 -0400

    Update game.py

--- game.py
+++ game.py
@@ -9,35 +9,35 @@ options = ["rock", "paper", "scissors"]
 if usermove not in options:
     print("User did not make a valid choice")
     print("Try again, selecting either 'rock', 'paper', or 'scissors'")
+else:
+    #DETERMINE CPU CHOICE Randomly
+    random.shuffle(options)
+    cpumove = random.choice(options)
+    print("The computer chose", cpumove)
 
-#DETERMINE CPU CHOICE Randomly
-random.shuffle(options)
-cpumove = random.choice(options)
-print("The computer chose", cpumove)
+    #DETERMINE WINNER BASED ON CHOICES
 
-#DETERMINE WINNER BASED ON CHOICES
-
-if cpumove == usermove:
-    print("User and computer both chose", usermove)
-    print("Please try again to determine a winner")
-elif cpumove == "rock":
-    if usermove == "paper":
-        print("User chose paper. Computer chose rock. User wins!")
-        print("Please play again!")
-    elif usermove == "scissors":
-        print("User chose scissors. Computer chose rock. Computer wins!")
-        print("Please play again!")
-elif cpumove == "paper":
-    if usermove == "rock":
-        print("User chose rock. Computer chose paper. Computer wins!")
-        print("Please play again!")
-    elif usermove == "scissors":
-        print("User chose scissors. Computer chose paper. User wins!")
-        print("Please play again!")
-elif cpumove == "scissors":
-    if usermove == "paper":
-        print("User chose paper. Computer chose scissors. Computer wins!")
-        print("Please play again!")
-    elif usermove == "rock":
-        print("User chose rock. Computer chose scissors. User wins!")
-        print("Please play again!")
+    if cpumove == usermove:
+        print("User and computer both chose", usermove)
+        print("Please try again to determine a winner")
+    elif cpumove == "rock":
+        if usermove == "paper":
+            print("User chose paper. Computer chose rock. User wins!")
+            print("Please play again!")
+        elif usermove == "scissors":
+            print("User chose scissors. Computer chose rock. Computer wins!")
+            print("Please play again!")
+    elif cpumove == "paper":
+        if usermove == "rock":
+            print("User chose rock. Computer chose paper. Computer wins!")
+            print("Please play again!")
+        elif usermove == "scissors":
+            print("User chose scissors. Computer chose paper. User wins!")
+            print("Please play again!")
+    elif cpumove == "scissors":
+        if usermove == "paper":
+            print("User chose paper. Computer chose scissors. Computer wins!")
+            print("Please play again!")
+        elif usermove == "rock":
+            print("User chose rock. Computer chose scissors. User wins!")
+            print("Please play again!")
